e89e034703242dad6eeed1b9ae0c844f03250d5a
Alexey Nichiporchik <lexarx@gmail.com>
2016-08-12 16:04:56 +0100

    Converted to CommonJS module format.

--- event.js
+++ event.js
@@ -1,37 +1,37 @@
-define('event', [
-	'class'
-], function(Class) {
-	return Class.extend({
-		add: function(callback, context) {
-			if (this.handlers === undefined) {
-				this.handlers = [];
-			}
-			this.handlers.push({
-				callback: callback,
-				context: context
-			});
-		},
+var Class = require('class');
 
-		remove: function(callback, context) {
-			if (this.handlers === undefined) {
-				return;
-			}
-			for (var i = this.handlers.length - 1; i >= 0; i--) {
-				if (this.handlers[i].callback === callback && 
-					this.handlers[i].context === context) {
-					this.handlers.splice(i, 1);
-				}
-			}
-		},
+var Event = Class.extend({
+	add: function(callback, context) {
+		if (this.handlers === undefined) {
+			this.handlers = [];
+		}
+		this.handlers.push({
+			callback: callback,
+			context: context
+		});
+	},
 
-		trigger: function() {
-			if (this.handlers === undefined) {
-				return;
-			}
-			var handlers = this.handlers.slice();
-			for (var i = 0; i < handlers.length; i++) {
-				handlers[i].callback.apply(handlers[i].context, arguments);
+	remove: function(callback, context) {
+		if (this.handlers === undefined) {
+			return;
+		}
+		for (var i = this.handlers.length - 1; i >= 0; i--) {
+			if (this.handlers[i].callback === callback && 
+				this.handlers[i].context === context) {
+				this.handlers.splice(i, 1);
 			}
 		}
-	});
+	},
+
+	trigger: function() {
+		if (this.handlers === undefined) {
+			return;
+		}
+		var handlers = this.handlers.slice();
+		for (var i = 0; i < handlers.length; i++) {
+			handlers[i].callback.apply(handlers[i].context, arguments);
+		}
+	}
 });
+
+module.exports = Event;
